41d64bc73d7a335912029caa61b4a8d1f8e4caf3
Dmitrii K <dm-kamaev@rambler.ru>
2016-08-11 12:00:53 +0300

    SV-169 added field fundInfo in model orderService and save fundInfo;

--- app/modules/orders/services/orderService.js
+++ app/modules/orders/services/orderService.js
@@ -4,12 +4,14 @@ const await = require('asyncawait/await');
 const async = require('asyncawait/async');
 const sequelize = require('../../../components/sequelize');
 
-exports.createPay = function(SberUserUserFundId, amount, listDirTopicFunds, listFunds) {
+
+exports.insertPay = function(SberUserUserFundId, amount, listDirTopicFunds, listFunds, fundInfo) {
     return await(sequelize.models.Order.create({
         SberUserUserFundId,
         amount,
         listDirTopicFunds,
         listFunds,
+        fundInfo,
     }));
 };
 
