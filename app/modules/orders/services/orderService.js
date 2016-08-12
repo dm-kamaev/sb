3208dfde28a8526cdec3ce162513c98fe784683e
Dmitrii K <dm-kamaev@rambler.ru>
2016-08-12 11:56:57 +0300

    SV-171 refactoring;

--- app/modules/orders/services/orderService.js
+++ app/modules/orders/services/orderService.js
@@ -2,16 +2,26 @@
 
 const await = require('asyncawait/await');
 const async = require('asyncawait/async');
-const sequelize = require('../../../components/sequelize');
+const sequelize       = require('../../../components/sequelize');
+const entityService   = require('../../entity/services/entityService');
 
 
-exports.insertPay = function(SberUserUserFundId, amount, listDirTopicFunds, listFunds, fundInfo) {
+/**
+ * create order in our base
+ * @param  {[int]}      data.SberUserUserFundId
+ * @param  {[int]}      data.amount
+ * @param  {[array]}    data.listDirectionTopicFunds [ [ 'fund', 'МОЙ ФОНД' ], [ 'topic', 'Рак крови' ] ]
+ * @param  {[array]}    data.listFunds [ 'МОЙ ФОНД', 'ПОДАРИ ЖИЗНь', 'МОЙ ФОНД' ]
+ * @param  {[array]}    data.fundInfo  [ entities as in database ]
+ * @return {[object]}   [ get id insert ]
+ */
+exports.insertPay = function (data) {
     return await(sequelize.models.Order.create({
-        SberUserUserFundId,
-        amount,
-        listDirTopicFunds,
-        listFunds,
-        fundInfo,
+        SberUserUserFundId:     data.SberUserUserFundId,
+        amount:                 data.amount,
+        listDirectionTopicFunds:data.listDirectionTopicFunds,
+        listFunds:              data.listFunds,
+        fundInfo:               data.fundInfo,
     }));
 };
 
@@ -46,9 +56,30 @@ function updateInfo(orderNumber, data) {
 exports.updateInfo = updateInfo;
 
 
+/**
+ * get array with Direction,Topic,Funds and array with funds, where
+ * Direction,Topic convert to Funds
+ * @param  {[array]} entities [description]
+ * @return [ [ 'fund', 'МОЙ ФОНД' ], [ 'topic', 'Рак крови' ] ] [ 'МОЙ ФОНД', 'ПОДАРИ ЖИЗНь', 'МОЙ ФОНД' ]
+ */
+exports.getListDirectionTopicFunds = function (entities) {
+    var listDirectionTopicFunds = [], listFunds = [];
+    for (var i = 0, l = entities.length; i < l; i++) {
+        var entity = entities[i].dataValues, type = entity.type;
+        listDirectionTopicFunds.push([type, entity.title]);
+        if (type === 'direction' || type === 'topic') {
+            listFunds = listFunds.concat(await(entityService.getFundsName(entity.id)));
+        } else {
+            listFunds.push(entity.title);
+        }
+    }
+    return [ listDirectionTopicFunds, listFunds ];
+};
+
+
 exports.handlerResponceSberAcqu = function(orderNumber, responceSberAcqu) {
     if (responceSberAcqu.orderId && responceSberAcqu.formUrl) {
-        await(orderService.updateInfo(orderNumber, { orderId: responceSberAcqu.orderId }));
+        await(updateInfo(orderNumber, { orderId: responceSberAcqu.orderId }));
         return responceSberAcqu;
     } else {
         const ourErrorCode    = '100'; // "100"(our code not sberbank) if sberbank acquiring is changed key's name in responce object
