e6ea6b39437609c1ec7d6d3cb43d28e1fefcd13d
Pavel Romanov <azzangetsu@gmail.com>
2016-08-30 15:04:58 +0300

    SV-249 Emulator migrations script fix

--- sber-emulator/app/components/sequelize/migrationsWrapper.js
+++ sber-emulator/app/components/sequelize/migrationsWrapper.js
@@ -3,9 +3,9 @@ const MigrationExecutor = require('nodules/sequelize/MigrationExecutor.js');
 
 var paths = {
     projectRoot: path.resolve(__dirname, '../../..'),
-    migrations: '/migrations',
-    migrationExecute: 'app/components/sequelize/tmp'
+    migrations: 'migrations',
+    migrationExecute: 'app/components/sequelize/tmp',
+    sequelizeBin: '../node_modules/.bin/sequelize'
 };
 
 module.exports = new MigrationExecutor(paths);
-
