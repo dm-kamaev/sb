7bd97d2be2c124df4d60386784f9ef6ad442ada7
Max Rylkin <msrylkin@gmail.com>
2016-10-06 16:27:27 +0300

    SV-392 some refactoring

--- app/components/server/middleware/metaTags.js
+++ app/components/server/middleware/metaTags.js
@@ -10,6 +10,8 @@ const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 const sizeOf = require('image-size');
 const path = require('path');
+const config = require('../../../../config/config');
+const BASE_URL = `http://${config.hostname}:${config.port}`
 
 module.exports = (req, res, next) => {
     var ua = req.headers['user-agent'];
@@ -23,37 +25,53 @@ module.exports = (req, res, next) => {
 
 nonSPARouter.get('/', async((req, res) => {
     if (req.query.entity) {
-        var entity = await(
-          entityService.getEntity(req.query.entity, null, true)
-        );
-        if (entity) {
-            var renderedEntity = entityView.renderEntity(entity);
-            var dimensions = sizeOf(path.join(__dirname,
-                    '../../../../public/uploads', entity.imgUrl));
-            return res.render('layout', {
-                imageUrl: renderedEntity.imgUrl,
-                imageWidth: dimensions.width,
-                imageHeight: dimensions.height,
-                url: `http://www58.lan:3000/#card?id=${req.query.entity}`,
-                title: renderedEntity.title,
-                descriptionText: renderedEntity.description
-            });
-        }
+        return handleEntity_(req, res)
     } else if (req.query.userFund) {
-        var userFund = await(userFundService.getUserFund(req.query.userFund));
-        if (userFund) {
-            var renderedUserFund = entityView.renderUserFund(userFund);
-            return res.render('layout', {
-                // imageUrl: ... ,
-                url: `http://www58.lan:3000/#card?id=${req.query.userFund}`,
-                title: renderedUserFund.title,
-                descriptionText: renderedUserFund.description
-            });
-        }
+        return handleUserFund_(req, res)
     }
     res.render('layout', {
-        url: 'http://www58.lan:3000/',
+        url: BASE_URL,
         title: 'Сбербанк вместе',
         descriptionText: 'Начните помогать осознанно.'
     });
 }));
+
+function handleEntity_(req, res) {
+    var entity = entityService.getEntity(req.query.entity, null, true, [])
+    if (entity) {
+        var renderedEntity = entityView.renderEntity(entity);
+        var imagePath = path.join(__dirname,
+            '../../../../public/uploads', entity.imgUrl);
+        calculateSize_(imagePath, (err, dimensions) => {
+            if (err) dimensions = {};
+            return res.render('layout', {
+                imageUrl: renderedEntity.imgUrl,
+                imageWidth: dimensions.width,
+                imageHeight: dimensions.height,
+                url: `${BASE_URL}/#card?id=${req.query.entity}`,
+                title: renderedEntity.title,
+                descriptionText: renderedEntity.description
+            });
+        })
+    }
+}
+
+function handleUserFund_(req, res) {
+    var userFund = userFundService.getUserFund(req.query.userFund)
+    if (userFund) {
+        var renderedUserFund = entityView.renderUserFund(userFund);
+        return res.render('layout', {
+            // imageUrl: ... ,
+            url: `${BASE_URL}/#card?id=${req.query.userFund}`,
+            title: renderedUserFund.title,
+            descriptionText: renderedUserFund.description
+        });
+    }
+}
+
+function calculateSize_(imagePath, cb) {
+    new Promise((resolve, reject) => {
+            resolve(sizeOf(imagePath))
+        })
+        .then(result => cb(null, result), err => cb(err))
+}
