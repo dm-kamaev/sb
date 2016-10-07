316348fb03a340e783702f79039b8598933ee190
Dmitrii K <dm-kamaev@rambler.ru>
2016-10-07 18:01:54 +0300

    SV-389: added single app for development;;

--- app/app.js
+++ app/app.js
@@ -5,19 +5,28 @@ const PORT = process.env.PORT || 3000;
 const log = require('./components/logger').getLogger('main');
 const cluster = require('cluster');
 
-// server.listen(PORT);
-
-if (cluster.isMaster) {
-    var workerNum = require('os').cpus().length;
-    log.info('Starting %s workers', workerNum);
-    for (var i = 0; i < workerNum; i++) {
-        cluster.fork();
-    }
-    
-    cluster.on('exit', function(worker) {
-        cluster.fork()
-    })
-} else {
+const NODE_ENV = process.env.NODE_ENV;
+if (NODE_ENV === 'development') {
     server.listen(PORT);
     log.info(`Server started at port ${PORT}`);
+} else {
+    clustering();
+}
+
+
+function clustering () {
+    if (cluster.isMaster) {
+        var workerNum = require('os').cpus().length;
+        log.info('Starting %s workers', workerNum);
+        for (var i = 0; i < workerNum; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', function(worker) {
+            cluster.fork()
+        })
+    } else {
+        server.listen(PORT);
+        log.info(`Server started at port ${PORT}`);
+    }
 }
