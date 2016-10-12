7d6f9e7ef77296f69cf89660b8a41a287230d7cd
Dmitrii K <dm-kamaev@rambler.ru>
2016-10-12 13:18:40 +0300

    SV-406: added new method and added check keys;

--- app/modules/auth/services/passwordAuth.js
+++ app/modules/auth/services/passwordAuth.js
@@ -11,18 +11,25 @@ const util   = require('util');
 module.exports = class PasswordAuth {
     /**
      * [constructor description]
-     * @param  {[obj]} params { ctx: "object from express" }
+     * @param  {[obj]} params {
+     *   // object from express
+     *   ctx: { request, response }
+     *  }
      * @return {[type]}        [description]
      */
     constructor (params) {
-        if (!params.ctx) {
-            throw new Error('PasswordAuth: not exist ctx '+params.ctx);
-        } else if (!params.ctx.request) {
-            throw new Error('PasswordAuth: not exist request in ctx '+params.ctx.request);
-        } else {
-            this.request = params.ctx.request;
+        if (!params.ctx) { throw new Error('PasswordAuth: not exist ctx '+params.ctx);}
+        var ctx = params.ctx;
+        if (!ctx.request) {
+            throw new Error('PasswordAuth: not exist request in ctx '+ctx.request);
         }
+        if (!ctx.response) {
+            throw new Error('PasswordAuth: not exist response in ctx '+ctx.response);
+        }
+        this.request  = ctx.request;
+        this.response = ctx.response;
     }
+
 
     /**
      * authorization via passport.js
@@ -41,5 +48,22 @@ module.exports = class PasswordAuth {
         } catch (err) {
             return { resolve:false, message: err.message || err };
         }
+    }
+
+
+    logout() {
+        var request = this.request;
+        return request.logout();
+    }
+
+
+    /**
+     * redirect only if request skip SPA
+     * @param  {[str]} url
+     * @return {[type]}
+     */
+    redirect(url) {
+        var response = this.response;
+        response.redirect(url);
     }
 };
