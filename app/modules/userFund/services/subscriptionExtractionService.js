dc4e3570575c5d07d263874d4ec6081347af67fe
Pavel Romanov <azzangetsu@gmail.com>
2016-10-17 21:39:40 +0300

    SV-470 extractor module added

---
+++ app/modules/userFund/services/subscriptionExtractionService.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const await = require('asyncawait/await');
+const async = require('asyncawait/async');
+const sequelize = require('../../../components/sequelize');
+const errors = require('../../../components/errors');
+const i18n = require('../../../components/i18n');
+const logger = require('../../../components/logger').getLogger('main');
+
+var extractionService = {};
+
+/**
+ * get array of userfunds, subscribed to one+ of presented entities
+ * @param {array} entityIds array of id of entities
+ * @return {array} userFundIds array of id of user funds
+ */
+extractionService.getSubscribedUserFunds = function(entityIds) {
+    var userFundIds = await(sequelize.sequelize.query(`
+        SELECT DISTINCT "UserFundEntity"."userFundId"
+        FROM "UserFundEntity"
+        WHERE "UserFundEntity"."entityId" IN (${entityIds})`, {
+            type: sequelize.sequelize.QueryTypes.SELECT
+        }));
+    return userFundIds.map(id => id.userFundId);
+}
+
+module.exports = extractionService;
