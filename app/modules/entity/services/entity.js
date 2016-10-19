7bf619ec2a137752f7628a6b0e6f9da3ea459076
Dmitrii K <dm-kamaev@rambler.ru>
2016-10-19 13:58:26 +0300

    SV-483: added module for work with entity;

--- app/modules/entity/services/entity.js
+++ app/modules/entity/services/entity.js
@@ -6,33 +6,60 @@
 const sequelize = require('../../../components/sequelize');
 const await = require('asyncawait/await');
 const async = require('asyncawait/async');
+const errors = require('../../../components/errors');
+const i18n = require('../../../components/i18n');
 const entityTypes = require('../../entity/enums/entityTypes.js');
 
 
+
 module.exports = class Entity {
-  constructor (params) {
-    this.entityId = params.entityId || null;
-    var option = params.option || {};
-    this.handlerError = option.handlerError || false;
-  }
-
-  getEntityId (entityId) {
-    entityId = entityId || this.entityId;
-    var entity = entityService.getEntity({ id:entityId, published:true });
-    throw new errors.NotFoundError(i18n.__('Entity'), entityId);
-    if (this.handlerError) {
-
-    } else {
-      if (!entity) { throw new errors.NotFoundError(i18n.__('Entity'), entityId); }
+    /**
+     * [constructor description]
+     * @return {[type]}        [description]
+     */
+    constructor(params) {
+        this.entityId = params.entityId || null;
+        var option = params.option || {};
+        this.Entity = sequelize.models.Entity;
     }
 
-      if (this.handlerError) {
-
-      } else {
-
-      }
+    setEntity(entity) {
+        this.entity = entity;
     }
-  }
 
+    getEntity(where) {
+        return await (this.Entity.findOne({
+            where
+        }));
+    }
 
+    /**
+     * check exist published entity
+     * @param  {[int]} entityId, // optional param
+     * @return {[obj]} { id: 1, title: 'ПОДАРИ ЖИЗНЬ', ... }
+     */
+    checkExist(entityId) {
+        entityId = entityId || this.entityId;
+        var entity = this.getEntity({ id: entityId, published: true });
+        if (!entity) {
+            throw new errors.NotFoundError(i18n.__('Entity'), entityId);
+        }
+        this.setEntity(entity);
+        return entity;
+    }
+
+    /**
+     * check type entity
+     * @return {[boolen]}
+     */
+    checkType() {
+        var entity = this.entity || {};
+        var type = entity.type;
+        if (!entityTypes[type]) {
+            throw new errors.NotFoundError(i18n.__('type of organization "{{type}}"', {
+                type
+            }), entity.id);
+        }
+        return true;
+    }
 };
