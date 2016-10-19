6fe85a1d8aba72509e4b63df9a0ab0e230565146
Max Rylkin <msrylkin@gmail.com>
2016-10-19 15:33:37 +0300

    SV-468 added email html

--- app/modules/mail/services/templateService.js
+++ app/modules/mail/services/templateService.js
@@ -7,6 +7,9 @@ const await = require('asyncawait/await')
 const async = require('asyncawait/async')
 const EmailTemplate = require('email-templates').EmailTemplate
 const templatesPath = path.join(__dirname, '../../../../public/mail_templates')
+const config = require('../../../../config/config')
+const HOSTNAME = `${config.hostname.replace(/\/+$/, '')}:${config.port}`
+const STATIC_PATH = `${HOSTNAME}/mail`
 const titles = {};
 module.exports = fs.readdirSync(templatesPath).reduce((obj, template) => {
       obj[template] = buildTemplate_.bind(null, path.join(templatesPath, template), template)
@@ -16,6 +19,10 @@ module.exports = fs.readdirSync(templatesPath).reduce((obj, template) => {
 
 function buildTemplate_(templateDir, templateName, locals) {
     var template = new EmailTemplate(templateDir)
+    Object.assign(locals, {
+        linkToStatic: STATIC_PATH,
+        cancelSubscriptionLink: 'http://yandex.ru'
+    })
     var rendered = await(new Promise((resolve, reject) => {
         template.render(locals, (err, result) => {
             if (err) return reject(err)
