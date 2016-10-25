ce9ce030138055945a7d2d202b02506c6c3f23f4
Max Rylkin <msrylkin@gmail.com>
2016-10-25 14:42:44 +0300

    SV-518 added pics to emails

--- app/components/server/middleware/hideRoutes.js
+++ app/components/server/middleware/hideRoutes.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const NotFoundError = require('../../errors/NotFoundError');
+const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = function(req, res, next) {
-    if (process.env.NODE_ENV == 'development') next()
+    if (NODE_ENV == 'development') next()
     else throw new NotFoundError('Not Found');
 }
