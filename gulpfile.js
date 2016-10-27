1a7d291cc0d7603def56dba73f4c7b44c0e07dd5
Pavel Romanov <azzangetsu@gmail.com>
2016-10-27 18:35:26 +0300

    SV-533 Gulp args fix

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('buildFrontend', () => {
     var gulpArgs = [
         `--environment="${environment}"`,
         `--modulesPath="${sberModules}"`,
-	      `--production`,
+	`--"${config.environment}"`,
         `--apiAddress="${config.hostname}"`,
         `--cwd ${sberModules}/${frontDirName} build`
     ];
